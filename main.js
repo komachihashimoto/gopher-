c0b96516a0b4d53a8a65f88a0b350c75c654bbb5
komachihashimoto <joujinameko@gmail.com>
2023-12-27 18:38:47 +0900

    手足の動きとリンク

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 'use strict';
 
-let animation = anime({
+let animation1 = anime({
   targets: '.container',
   translateX: 500, 
   duration: 2000,
   easing: 'linear',
   loop: true,
   direction: 'alternate',
-  autoplay: false, // アニメーションの自動再生を無効にします
 });
+
+let animation2 = anime({
+  targets: '.l-hand, .r-foot',
+  translateY: ['-3px', '3px'],
+  duration: 300,
+  easing: 'easeInOutSine',
+  direction: 'alternate',
+  loop: true,
+});
+
+let animation3 = anime({
+  targets: '.r-hand, .l-foot',
+  translateY: ['3px', '-3px'],
+  duration: 300,
+  easing: 'easeInOutSine',
+  direction: 'alternate',
+  loop: true,
+});
+
+
 
 function playJump() {
   let Jump = document.getElementById('Jump');
@@ -19,7 +38,7 @@ function playJump() {
 function handleKeyUp(e){
   if(e.keyCode == 32){
       document.body.removeEventListener('keyup', handleKeyUp); // スペースキーが押されたときにイベントリスナーを削除します
-      animation.pause(); // スペースキーが押されたときにアニメーションを一時停止します
+      (animation1, animation2, animation3).pause(); // スペースキーが押されたときにアニメーションを一時停止します
       anime({
           targets: '.container > :not(.shadow)', // '.shadow'クラスを除外します
           keyframes: [
@@ -27,7 +46,7 @@ function handleKeyUp(e){
               {translateY: 0, duration: 300, easing:'easeInQuad'}
           ],
           complete: function() { // アニメーションが完了したら
-            animation.play(); // 元のアニメーションを再開します
+            (animation1, animation2, animation3).play(); // 元のアニメーションを再開します
             document.body.addEventListener('keyup', handleKeyUp); // アニメーションが完了したらイベントリスナーを再度追加します
           }
       });
