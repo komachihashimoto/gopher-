5c04148cfad7e20479328fac8441f9c893dbb97f
komachihashimoto <joujinameko@gmail.com>
2023-12-28 15:49:31 +0900

    横移動の削除

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 'use strict';
 
-let animation1 = anime({
-  targets: '.container',
-  translateX: 1000, 
-  duration: 2000,
-  easing: 'linear',
-  loop: true,
-  direction: 'alternate',
-  autoplay: true,
-});
 
 let animation2 = anime({
   targets: '.l-hand, .r-foot',
@@ -41,23 +32,23 @@ function playJump() {
 function handleKeyUp(e){
   if(e.keyCode == 32){
       document.body.removeEventListener('keyup', handleKeyUp); // スペースキーが押されたときにイベントリスナーを削除します
-      (animation1, animation2, animation3).pause(); // スペースキーが押されたときにアニメーションを一時停止します
+      (animation2, animation3).pause(); // スペースキーが押されたときにアニメーションを一時停止します
       anime({
           targets: '.container > :not(.shadow)', // '.shadow'クラスを除外します
           keyframes: [
-              {translateY: -300, duration: 300, easing:'easeOutQuad'},
-              {translateY: 0, duration: 300, easing:'easeInQuad'}
+              {translateY: -500, duration: 400, easing:'easeOutQuad'},
+              {translateY: 0, duration: 400, easing:'easeInQuad'}
           ],
           complete: function() { // アニメーションが完了したら
-            (animation1, animation2, animation3).play(); // 元のアニメーションを再開します
+            (animation2, animation3).play(); // 元のアニメーションを再開します
             document.body.addEventListener('keyup', handleKeyUp); // アニメーションが完了したらイベントリスナーを再度追加します
           }
       });
       anime({
         targets: '.shadow',
         keyframes: [
-            {translateX: 300, duration: 300, easing:'easeOutQuad'},
-            {translateX: 0, duration: 300, easing:'easeInQuad'}
+            {translateX: 500, duration: 400, easing:'easeOutQuad'},
+            {translateX: 0, duration: 400, easing:'easeInQuad'}
         ]
     });
       playJump();
@@ -66,4 +57,4 @@ function handleKeyUp(e){
 
 document.body.addEventListener('keyup', handleKeyUp); // イベントリスナーを追加します
 
-(animation1, animation2, animation3).play(); // 初期アニメーションの再生を開始します
+(animation2, animation3).play(); // 初期アニメーションの再生を開始します
