4e90cd63d713a60e5c0265690d2c3eda31ddcbd8
komachihashimoto <joujinameko@gmail.com>
2024-02-04 09:32:03 +0900

    仕上げ

--- main.js
+++ main.js
@@ -3,33 +3,6 @@
 const Btn = document.getElementById('Btn');
 let   s   = true;
 
-// let jump = anime({
-//   targets: '.container > :not(.shadow)', // '.shadow'クラスを除外します
-//   keyframes: [
-//       {translateY: -500, duration: 400, easing:'easeOutQuad'},
-//       {translateY: 0, duration: 400, easing:'easeInQuad'}
-//   ],
-// });
-
-// let jumps = anime({
-//   targets: '.shadow',
-//   keyframes: [
-//       {translateX: 500, duration: 400, easing:'easeOutQuad'},
-//       {translateX: 0, duration: 400, easing:'easeInQuad'}
-//   ]
-// });
-
-
-// let animation2 = anime({
-//   targets: '.l-hand, .r-foot',
-//   translateY: ['-3px', '3px'],
-//   duration: 300,
-//   easing: 'easeInOutSine',
-//   direction: 'alternate',
-//   loop: true,
-//   autoplay: true,
-// });
-
 
 function playJump() {
   let Jump = document.getElementById('Jump');
@@ -42,6 +15,7 @@ Btn.addEventListener (
   function jumping() {
     if(s == true) {
       s = false;
+      playJump();
       anime({
         targets: '.container > :not(.shadow)', // '.shadow'クラスを除外します
         keyframes: [
@@ -59,31 +33,7 @@ Btn.addEventListener (
           s = true;
         }
       });
-      playJump();
     }
   }
 );
 
-// function handleKeyUp(e){
-//   if(e.keyCode == 32){
-//       document.body.removeEventListener('keyup', handleKeyUp); // スペースキーが押されたときにイベントリスナーを削除します
-//       // (animation2, animation3).pause(); // スペースキーが押されたときにアニメーションを一時停止します
-//       anime({
-//           targets: '.container > :not(.shadow)', // '.shadow'クラスを除外します
-//           keyframes: [
-//               {translateY: -500, duration: 400, easing:'easeOutQuad'},
-//               {translateY: 0, duration: 400, easing:'easeInQuad'}
-//           ],
-//           complete: function() { // アニメーションが完了したら
-//             // (animation2, animation3).play(); // 元のアニメーションを再開します
-//             document.body.addEventListener('keyup', handleKeyUp); // アニメーションが完了したらイベントリスナーを再度追加します
-//           }
-//       });
-//       anime({
-//         targets: '.shadow',
-//         keyframes: [
-//             {translateX: 500, duration: 400, easing:'easeOutQuad'},
-//             {translateX: 0, duration: 400, easing:'easeInQuad'}
-//         ]
-//     });
-//   }};
